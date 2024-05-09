e739ab204e5aaa7346be306087959456fd93bc9a
mecrophagus <122439515+mecrophagus@users.noreply.github.com>
2024-05-09 20:24:21 +0000

    push

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function setExcuse() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let randomWho = who[Math.floor(Math.random() * who.length)];
+    let randomAction = action[Math.floor(Math.random() * action.length)];
+    let randomWhat = what[Math.floor(Math.random() * what.length)];
+    let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    let excuse =
+      randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
+
+    document.getElementById("excuse").innerHTML = excuse;
+  }
+
+  setExcuse(); // Llama a la función setExcuse() para establecer una excusa al cargar la página
 };
